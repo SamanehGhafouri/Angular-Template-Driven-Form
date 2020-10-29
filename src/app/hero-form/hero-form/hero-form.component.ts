a967903c1868682e90e0b971e8129906137106f2
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-10-29 16:23:44 -0400

    enable Forms, display form in root template

--- src/app/hero-form/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form/hero-form.component.ts
@@ -10,10 +10,12 @@ export class HeroFormComponent{
   powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
   submitted = false;
+
   onSubmit(){
     this.submitted = true;
   }
   get diagnostic() { return JSON.stringify(this.model);}
+
 
 
 
